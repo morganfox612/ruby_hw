811ebbe185bda695a02cc42e72f1af581e637c3b
Morgan Fox <morganfox61297@gmail.com>
2019-06-06 13:04:11 -0400

    hw

--- instance.rb
+++ instance.rb
@@ -1,11 +1,14 @@
 puts "Please enter a string"
-count = 0
-string = gets.chomp
-x = string.split('')
+$count = 0
+$string = gets.chomp
+$x = $string.split('')
 def most_often
-  x.each do |y|
-    arr = string.scan(y)
-    count = arr.length
+  $x.each do |y|
+    arr = $string.scan(y)
+    if arr.length > $count
+      $count = arr.length
+    end
   end
-  puts count
+  puts $count
 end
+most_often
